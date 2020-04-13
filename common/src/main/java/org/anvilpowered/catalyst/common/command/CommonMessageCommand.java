81b42a4eb93542d336e278e0840a9b984c0d3c49
STG-Allen <allenclan23@gmail.com>
2020-04-12 21:32:20 -0400

    Removed commented lines

--- common/src/main/java/org/anvilpowered/catalyst/common/command/CommonMessageCommand.java
+++ common/src/main/java/org/anvilpowered/catalyst/common/command/CommonMessageCommand.java
@@ -63,9 +63,6 @@ public class CommonMessageCommand<
             args[0] = "";
             String message = String.join(" ", args);
 
-            //memberManger.asQueryForUser() == null
-            //MemberManger.asQueryForNick(name)
-
             if (isConsole) {
                 privateMessageService.sendMessage("Me", name, message);
                 privateMessageService.sendMessage("Console", "Me", message);
