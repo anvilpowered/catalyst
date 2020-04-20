502faaf1031a4fd0e390dfb931b618bb19d3ec41
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 13:28:29 +0100

    Changed behaviour to also address spaces separating swears.

--- common/src/main/java/org/anvilpowered/catalyst/common/listener/CommonChatListener.java
+++ common/src/main/java/org/anvilpowered/catalyst/common/listener/CommonChatListener.java
@@ -87,17 +87,14 @@ public class CommonChatListener<
         }
 
         Optional<Channel> channel = chatService.getChannelFromId(chatService.getChannelIdForUser(playerUUID));
-        List<String> swearList = chatFilter.isSwear(message);
         message = chatService.checkPlayerName(message);
 
         if (channel.isPresent()) {
-            if (swearList != null) {
-                if (!permissionService.hasPermission((TSubject) player, registry.getOrDefault(CatalystKeys.LANGUAGE_ADMIN))) {
-                    for (String swear : swearList) {
-                        message = message.replace(swear, "****");
-                    }
-                }
+
+            if (!permissionService.hasPermission((TSubject) player, registry.getOrDefault(CatalystKeys.LANGUAGE_ADMIN))) {
+                message = chatFilter.replaceSwears(message);
             }
+
             chatEvent.setSender(player);
             chatEvent.setMessage(textService.of(message));
             chatEvent.setRawMessage(message);
