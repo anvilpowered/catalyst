5590fa32a5ac5556cfbfe1ebf7717a4705c6c29b
STG-Allen <ajacob715@gmail.com>
2021-06-24 09:02:24 -0400

    Update license to 2021 and fix spacing

--- api/src/main/java/org/anvilpowered/catalyst/api/registry/AdvancedServerInfo.java
+++ api/src/main/java/org/anvilpowered/catalyst/api/registry/AdvancedServerInfo.java
@@ -1,6 +1,6 @@
 /*
- * Catalyst - AnvilPowered
- *   Copyright (C) 2020
+ *   Catalyst - AnvilPowered
+ *   Copyright (C) 2021
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
--- common/src/main/kotlin/org/anvilpowered/catalyst/common/listener/JoinListener.kt
+++ common/src/main/kotlin/org/anvilpowered/catalyst/common/listener/JoinListener.kt
@@ -1,24 +1,24 @@
 /*
- *     Copyright (C) 2020 STG_Allen
+ *   Catalyst - AnvilPowered
+ *   Copyright (C) 2021
  *
  *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
+ *     it under the terms of the GNU Lesser General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     GNU Lesser General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see https://www.gnu.org/licenses/.
  */
 package org.anvilpowered.catalyst.common.listener
 
 import com.google.common.eventbus.Subscribe
 import com.google.inject.Inject
-import org.anvilpowered.anvil.api.misc.Named
 import org.anvilpowered.anvil.api.registry.Registry
 import org.anvilpowered.anvil.api.server.LocationService
 import org.anvilpowered.anvil.api.util.PermissionService
@@ -55,10 +55,12 @@ class JoinListener<TString, TPlayer, TCommandSource> @Inject constructor(
       privateMessageService.socialSpySet().add(playerUUID)
     }
     val userName = userService.getUserName(player as TPlayer)
-    var server =
-      if (registry.getOrDefault(CatalystKeys.ADVANCED_SERVER_INFO_ENABLED)) locationService.getServer(userName).map { obj: Named -> obj.name }
-        .orElse("null") else locationService.getServer(playerUUID).map { obj: Named -> obj.name }
-        .orElse("null")
+    var server = if (registry.getOrDefault(CatalystKeys.ADVANCED_SERVER_INFO_ENABLED)) {
+      locationService.getServer(userName).map { it.name }.orElse("null")
+    } else {
+      locationService.getServer(playerUUID).map { it.name }.orElse("null")
+    }
+
     if (registry.getOrDefault(CatalystKeys.ADVANCED_SERVER_INFO_ENABLED)) {
       serverService.insertPlayer(userName, serverService.getPrefix(virtualHost))
       server = serverService.getPrefixForPlayer(userName)
