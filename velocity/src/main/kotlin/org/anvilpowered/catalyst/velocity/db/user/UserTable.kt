47d5c18be10f1266253a56dafe4d6d6ddd8aca60
Alexander Städing <alexanderstaeding@gmail.com>
2023-11-25 15:29:35 +0100

    Get luckperms to work properly

--- velocity/src/main/kotlin/org/anvilpowered/catalyst/velocity/db/user/UserTable.kt
+++ velocity/src/main/kotlin/org/anvilpowered/catalyst/velocity/db/user/UserTable.kt
@@ -26,7 +26,7 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ResultRow
 import java.util.UUID
 
-internal object UserTable : UUIDTable("users") {
+internal object UserTable : UUIDTable("catalyst_users") {
     val username = varchar("username", 255).uniqueIndex()
     val email = varchar("email", 255).uniqueIndex().nullable()
     val discordUserId = long("discord_user_id").uniqueIndex().nullable()
